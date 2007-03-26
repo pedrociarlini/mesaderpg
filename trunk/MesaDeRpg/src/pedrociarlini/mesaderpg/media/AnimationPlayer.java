5cbd6c21a19106079afe980754e1b4c86335021b
pedrociarlini <pedrociarlini@6154dee7-f22a-0410-81ad-11548b01ce4d>
2007-03-26 22:22:15 +0000

    Inserindo classes de media e avançoes relacionados a comunicação de redes.

---
+++ trunk/MesaDeRpg/src/pedrociarlini/mesaderpg/media/AnimationPlayer.java
@@ -0,0 +1,83 @@
+package pedrociarlini.mesaderpg.media;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.media.CannotRealizeException;
+import javax.media.Control;
+import javax.media.GainControl;
+import javax.media.Manager;
+import javax.media.NoPlayerException;
+import javax.media.Player;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class AnimationPlayer {
+	public static void main(String[] args) {
+		try {
+			String file;
+			file = "file://"
+					+ "C:\\Pedro\\downloads\\mrkite.mp3"
+							.replaceAll("\\\\", "/");
+			/*file = "file://"
+				+ "C:\\Pedro\\Pessoal\\Music\\EtCircensis\\V005.WAV"
+						.replaceAll("\\\\", "/");
+			*/
+			/*
+			file = "file://"
+				+ "C:/Pedro/Meus arquivos recebidos/EmergencyLanding.in.a.hightway.avi"
+						.replaceAll("\\\\", "/");
+			*/
+			// Manager.setHint(Manager.LIGHTWEIGHT_RENDERER, true);
+			Player p = Manager.createRealizedPlayer(new URL(file));
+			for (Control control : p.getControls()) {
+				System.out.println(control);
+			}
+			JFrame janela = new JFrame("Teste de player");
+			JPanel panel = new JPanel();
+			janela.getContentPane().add(panel);
+			janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			panel.setLayout(new BorderLayout(10, 10));
+			if (p.getVisualComponent() != null) {
+				panel.add(p.getVisualComponent(), BorderLayout.CENTER);
+			}
+			panel.add(p.getControlPanelComponent(), BorderLayout.SOUTH);
+			final GainControl gainControl = p.getGainControl();
+			final JLabel labelVolume = new JLabel("" + gainControl.getLevel());
+			panel.add(labelVolume, BorderLayout.WEST);
+			
+			JSlider slider = new JSlider(0, 1000);
+			slider.setOrientation(JSlider.VERTICAL);
+			slider.addChangeListener(new ChangeListener(){
+				public void stateChanged(ChangeEvent e) {
+					gainControl.setDB(((JSlider)e.getSource()).getValue()/1000);
+					labelVolume.setText(""+gainControl.getLevel()/1000);
+					
+				}
+			});
+			panel.add((Component)p.getControls()[3], BorderLayout.EAST);
+			janela.setSize(400, 300);
+			janela.setVisible(true);
+			p.start();
+		} catch (NoPlayerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CannotRealizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
