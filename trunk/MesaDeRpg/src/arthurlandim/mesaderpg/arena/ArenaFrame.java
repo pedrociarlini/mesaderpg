c29473f9b0744356bb75d844c77881f52c782f17
arthurlandim <arthurlandim@6154dee7-f22a-0410-81ad-11548b01ce4d>
2007-05-31 04:51:18 +0000

    Frame da arena

---
+++ trunk/MesaDeRpg/src/arthurlandim/mesaderpg/arena/ArenaFrame.java
@@ -0,0 +1,47 @@
+package arthurlandim.mesaderpg.arena;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class ArenaFrame extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7587352830855485212L;
+
+	ArenaCanvas canvas;
+
+	public ArenaFrame() {
+		super();
+		Container container = getContentPane();
+
+		canvas = new ArenaCanvas();
+		container.add(canvas);
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(1, 2));
+		panel.add(new JLabel("x,y: ", JLabel.RIGHT));
+		container.add(panel, BorderLayout.SOUTH);
+
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		setSize(800, 600);
+		setVisible(true);
+	}
+
+	public static void main(String arg[]) {
+		new ArenaFrame();
+	}
+
+
+}
