43b98e615e34540e0459a8382ea105dcdf38b35e
arthurlandim <arthurlandim@6154dee7-f22a-0410-81ad-11548b01ce4d>
2007-05-31 04:52:21 +0000

    Acao de visualizar a arena

---
+++ trunk/MesaDeRpg/src/pedrociarlini/mesaderpg/ui/action/VisualizarArenaAction.java
@@ -0,0 +1,29 @@
+package pedrociarlini.mesaderpg.ui.action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+
+import pedrociarlini.mesaderpg.model.JogadorVO;
+import pedrociarlini.mesaderpg.ui.JanelaConfigurarJogador;
+
+public class VisualizarArenaAction extends AbstractAction {
+
+    public VisualizarArenaAction() {
+        putValue(Action.NAME, "VisualizarArena");
+        putValue(Action.MNEMONIC_KEY, KeyEvent.VK_J);
+        putValue(Action.LONG_DESCRIPTION, "Visualizar a Arena");
+    }
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public void actionPerformed(ActionEvent e) {
+        //System.out.println(jogador);
+    	new arthurlandim.mesaderpg.arena.ArenaFrame().setVisible(true);
+    }
+}
