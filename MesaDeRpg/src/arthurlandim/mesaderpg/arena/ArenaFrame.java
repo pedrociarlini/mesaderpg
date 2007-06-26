24866ddfb92026023c2be228ace8f36b969db36c
pedrociarlini <pedrociarlini@6154dee7-f22a-0410-81ad-11548b01ce4d>
2007-06-26 19:31:11 +0000

    Alteração da arena para adapter ao VE e para inserir jogadores.

--- MesaDeRpg/src/arthurlandim/mesaderpg/arena/ArenaFrame.java
+++ MesaDeRpg/src/arthurlandim/mesaderpg/arena/ArenaFrame.java
@@ -1,14 +1,16 @@
 package arthurlandim.mesaderpg.arena;
 
 import java.awt.BorderLayout;
-import java.awt.Container;
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import pedrociarlini.mesaderpg.model.JogadorVO;
+import javax.swing.WindowConstants;
 
 public class ArenaFrame extends JFrame implements ArenaInterface {
 	/**
@@ -18,22 +20,54 @@ public class ArenaFrame extends JFrame implements ArenaInterface {
 
 	ArenaCanvas canvas;
 
+	private JPanel panelComandos;
+
+	private JButton btInserir;
+
+	private JTextField textNome;
+
+	private JPanel principalPane;
+
 	public ArenaFrame() {
 		super();
-		Container container = getContentPane();
-
-		canvas = new ArenaCanvas();
-		container.add(canvas);
-
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(1, 2));
-		panel.add(new JLabel("x,y: ", JLabel.RIGHT));
-		container.add(panel, BorderLayout.SOUTH);
-		
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-
+		initialize();
+	}
+	
+	private void initialize() {
+		this.setContentPane(getPrincipalPane());
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		this.setTitle("Arena de jogadores");
 		setSize(640, 480);
 		setVisible(true);
+	}
+	
+	/**
+	 * This method initializes principalPane
+	 * 
+	 * @return javax.swing.JPanel
+	 */
+	private JPanel getPrincipalPane() {
+		if (principalPane == null) {
+			BorderLayout borderLayout = new BorderLayout(1, 0);
+			principalPane = new JPanel();
+			principalPane.setLayout(borderLayout);
+			principalPane.add(getCanvas(), BorderLayout.CENTER);
+
+			principalPane.add(getPanelComandos(), BorderLayout.SOUTH);
+
+		}
+		return principalPane;
+	}
+	
+	private JPanel getPanelComandos() {
+		if (panelComandos == null) {
+			panelComandos = new JPanel();
+			panelComandos.setLayout(new GridLayout(1, 3));
+			panelComandos.add(getTextNome());
+			panelComandos.add(getBtInserir());
+			panelComandos.add(new JLabel("x,y: ", JLabel.RIGHT));
+		}
+		return panelComandos;
 	}
 
 	public static void main(String arg[]) {
@@ -42,7 +76,6 @@ public class ArenaFrame extends JFrame implements ArenaInterface {
 
 	public void adicionarJogador(JogadorVO jogador) {
 		canvas.adicionarJogador(jogador);
-		
 	}
 
 	public void removerJogador(JogadorVO jogador) {
@@ -50,4 +83,31 @@ public class ArenaFrame extends JFrame implements ArenaInterface {
 		
 	}
 
+	public JButton getBtInserir() {
+		if (btInserir == null) {
+			btInserir = new JButton();
+			btInserir.setText("Inserir Jogador");
+			btInserir.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					adicionarJogador(new JogadorVO(getTextNome().getText(), false));
+					getTextNome().setText(getTextNome().getText() + " 2");
+				}
+			});
+		}
+		return btInserir;
+	}
+
+	public JTextField getTextNome() {
+		if (textNome == null) {
+			textNome = new JTextField();
+		}
+		return textNome;
+	}
+
+	public ArenaCanvas getCanvas() {
+		if(canvas == null) {
+			canvas = new ArenaCanvas();
+		}
+		return canvas;
+	}
 }
